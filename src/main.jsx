d59a3f85fce355e4da977680cd307fc04e8753b6
Searcher06 <ahmadibrahimsearcher@example.com>
2025-02-20 03:42:30 +0100

    deleted the fontawesome folder

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import "./recipestyle.css";
 import "../src/loader.css";
 import "../src/responsive.css"
-import "../fontawesome-free-6.6.0-web/css/all.css";
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
